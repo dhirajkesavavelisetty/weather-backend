1b622f5659896e9095d868d2e595892025591574
Dhiraj_247 <dhirajkesavavelisetty@gmail.com>
2026-07-04 17:59:39 +0530

    dropdown bugs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
@@ -14,105 +14,117 @@ app.use(cors());
 });
 */
 app.get("/", (req, res) => {
-
-    res.send("Backend is working");
-
+  res.send("Backend is working");
 });
 
 app.get("/search", async (req, res) => {
-    try {
-        const city = req.query.city;
-        if (!city) {
-            return res.json([]);
-        }
-
-        const response = await fetch(
-            `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10`
-        );
-
-        const data = await response.json();
-        if (!data.results) {
-            return res.json([]);
-        }
-
-        const suggestions = data.results.map((place) => ({
-            id: place.id,
-            name: place.name,
-            country: place.country,
-            state: place.admin1,
-            latitude: place.latitude,
-            longitude: place.longitude,
-            population: place.population,
-        }));
-
-        res.json(suggestions);
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch city suggestions" });
+  try {
+    const city = req.query.city;
+    if (!city) {
+      return res.json([]);
     }
+
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10`,
+    );
+
+    const data = await response.json();
+    if (!data.results) {
+      return res.json([]);
+    }
+
+    const suggestions = data.results.map((place) => ({
+      id: place.id,
+      name: place.name,
+      country: place.country,
+      state: place.admin1,
+      latitude: place.latitude,
+      longitude: place.longitude,
+      population: place.population,
+    }));
+
+    res.json(suggestions);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch city suggestions" });
+  }
 });
 
 app.get("/weather/coordinates", async (req, res) => {
-    try {
-        const { latitude, longitude, name, country } = req.query;
+  try {
+    const { latitude, longitude, name, country } = req.query;
 
-        if (!latitude || !longitude) {
-            return res.status(400).json({ error: "Latitude and longitude are required" });
-        }
-
-        const weatherResponse = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
-        );
-
-        const weatherData = await weatherResponse.json();
-
-        if (!weatherData.current) {
-            return res.status(404).json({ error: "Weather not found" });
-        }
-
-        res.json({
-            city: country ? `${name}, ${country}` : name,
-            temperature: weatherData.current.temperature_2m,
-            humidity: weatherData.current.relative_humidity_2m,
-            windSpeed: weatherData.current.wind_speed_10m,
-            weatherCode: weatherData.current.weather_code,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch weather data" });
+    if (!latitude || !longitude) {
+      return res
+        .status(400)
+        .json({ error: "Latitude and longitude are required" });
     }
+
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`,
+    );
+
+    const weatherData = await weatherResponse.json();
+
+    if (weatherData.error) {
+      return res.status(400).json({
+        error: weatherData.reason,
+      });
+    }
+
+    if (!weatherData.current) {
+      return res.status(404).json({
+        error: "Current weather data unavailable",
+      });
+    }
+
+    res.json({
+      city: country ? `${name}, ${country}` : name,
+      temperature: weatherData.current.temperature_2m,
+      humidity: weatherData.current.relative_humidity_2m,
+      windSpeed: weatherData.current.wind_speed_10m,
+      weatherCode: weatherData.current.weather_code,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
 });
 
 app.get("/weather/:city", async (req, res) => {
-    try{
-        const city = req.params.city;
-        const response= await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
+  try {
+    const city = req.params.city;
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`,
+    );
 
-        const data=await response.json();
-        if (!data.results || data.results.length === 0) {
-            return res.status(404).json({ error: "City not found" });
-        }
-
-        const latitude = data.results[0].latitude;
-        const longitude = data.results[0].longitude;
-
-        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`);
-        
-        const weatherData = await weatherResponse.json();
-        const cityName = data.results[0].name;
-
-        res.json({
-            city: cityName,
-            temperature: weatherData.current.temperature_2m,
-            humidity: weatherData.current.relative_humidity_2m,
-            windSpeed: weatherData.current.wind_speed_10m,
-            weatherCode: weatherData.current.weather_code,
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch weather data" });
+    const data = await response.json();
+    if (!data.results || data.results.length === 0) {
+      return res.status(404).json({ error: "City not found" });
     }
+
+    const latitude = data.results[0].latitude;
+    const longitude = data.results[0].longitude;
+
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`,
+    );
+
+    const weatherData = await weatherResponse.json();
+    const cityName = data.results[0].name;
+
+    res.json({
+      city: cityName,
+      temperature: weatherData.current.temperature_2m,
+      humidity: weatherData.current.relative_humidity_2m,
+      windSpeed: weatherData.current.wind_speed_10m,
+      weatherCode: weatherData.current.weather_code,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
 });
 
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
